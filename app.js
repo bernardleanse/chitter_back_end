87eff29a67946e82ece477b3a6bc62096d92d3c1
Bernard Leanse <bernardleanse96@gmail.com>
2022-01-03 11:45:26 +0000

    cors

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Chitter Back End listening at http://localhost:${port}`)
-})
+  console.log(`Chitter Back End listening at http://localhost:${port}`);
+});
