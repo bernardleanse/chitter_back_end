f4f500fe5bf5a350164f72229ab91c752f86abf8
Bernard Leanse <bernardleanse96@gmail.com>
2022-01-04 14:25:22 +0000

    prisma and simple json response from root

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(JSON.stringify('Hello World!'));
 });
 
 app.listen(port, () => {
   console.log(`Chitter Back End listening at http://localhost:${port}`);
 });
+
+app.post('/peeps', (req, res) => {
+  res.send("/peeps post generic response")
+})
