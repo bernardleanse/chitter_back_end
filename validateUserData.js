64a928460ec83acd98e7c52552d0541ef3ec1911
Bernard Leanse <bernardleanse96@gmail.com>
2022-02-01 18:09:27 +0000

    empty validate method

--- validateUserData.js
+++ validateUserData.js
@@ -1,5 +1,12 @@
-const validateUserData = () => {
-  return false
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+
+const isUsernameUnique = (username) => {
+  
+}
+
+const validateUserData = (data) => {
+  const validUsername = isUsernameUnique(data.username)
 }
 
 module.exports = validateUserData
